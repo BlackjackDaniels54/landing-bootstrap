a9f4294b75c48f595ae0d658b1cc85ca0e450e58
BlackjackDaniels54 <86078300+BlackjackDaniels54@users.noreply.github.com>
2022-07-20 11:40:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+function changeItem(){
+    document.getElementById('colorbgc').style.backgroundColor='#000';
+    document.getElementById('colorbgc').style.color='#fff';
+}
+function rechangeItem(){
+    document.getElementById('colorbgc').style.backgroundColor='#ffc451';
+    document.getElementById('colorbgc').style.color='#000';
+}
+
+function changeItem1(){
+    document.getElementById('colorbgc1').style.backgroundColor='#000';
+    document.getElementById('colorbgc1').style.color='#fff';
+}
+function rechangeItem1(){
+    document.getElementById('colorbgc1').style.backgroundColor='#ffc451';
+    document.getElementById('colorbgc1').style.color='#000';
+}
+
+function changeItem2(){
+    document.getElementById('colorbgc2').style.backgroundColor='#000';
+    document.getElementById('colorbgc2').style.color='#fff';
+}
+function rechangeItem2(){
+    document.getElementById('colorbgc2').style.backgroundColor='#ffc451';
+    document.getElementById('colorbgc2').style.color='#000';
+}
+
+function changeItem3(){
+    document.getElementById('colorbgc3').style.backgroundColor='#000';
+    document.getElementById('colorbgc3').style.color='#fff';
+}
+function rechangeItem3(){
+    document.getElementById('colorbgc3').style.backgroundColor='#ffc451';
+    document.getElementById('colorbgc3').style.color='#000';
+}
+
+function changeItem4(){
+    document.getElementById('colorbgc4').style.backgroundColor='#000';
+    document.getElementById('colorbgc4').style.color='#fff';
+}
+function rechangeItem4(){
+    document.getElementById('colorbgc4').style.backgroundColor='#ffc451';
+    document.getElementById('colorbgc4').style.color='#000';
+}
+
+function changeItem5(){
+    document.getElementById('colorbgc5').style.backgroundColor='#000';
+    document.getElementById('colorbgc5').style.color='#fff';
+}
+function rechangeItem5(){
+    document.getElementById('colorbgc5').style.backgroundColor='#ffc451';
+    document.getElementById('colorbgc5').style.color='#000';
+}
